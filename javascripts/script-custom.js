fa46aed83898f1e70f0162068d337e9fe2e080d9
rafek <rafal.legiedz@gmail.com>
2015-04-27 19:53:00 +0200

    Added speakers section

--- javascripts/script-custom.js
+++ javascripts/script-custom.js
@@ -3,5 +3,9 @@
         $(".primary-button-container li").click(function (e) {
             console.log(e);
         });
+
+        $(".btn-all-speakers").click(function (e) {
+            window.location.href = "speakers.html";
+        });
     });
 })(jQuery);
